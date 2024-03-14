9c1812a0c812231cb403371227dc8f8e0a35834e
Alexandre Brown <alexandrebrown97@gmail.com>
2024-03-14 10:36:36 -0400

    Updated exploration script

--- explore_env.py
+++ explore_env.py
@@ -1,50 +1,60 @@
 import hydra
 import torch
+import logging
 from omegaconf import DictConfig
 from pathlib import Path
 from torchrl.collectors.collectors import SyncDataCollector
 from torchrl.collectors.collectors import RandomPolicy
+from torchrl.record.loggers.csv import CSVLogger
+from torchrl.record import VideoRecorder
 from tqdm import tqdm
 from envs.env_factory import create_env
 
 
+logger = logging.getLogger(__name__)
+
+
 @hydra.main(version_base=None, config_path="configs/", config_name="env_exploration")
 def main(cfg: DictConfig):
-
-    video_dir = Path(hydra.core.hydra_config.HydraConfig.get().runtime.output_dir) / Path(cfg['logging']['video_dir'])
-    
     device = torch.device("cpu")
     
-    env = create_env(exp_name=cfg['experiment']['name'],
+    env = create_env(env_name=cfg['env']['name'],
                      seed=cfg['experiment']['seed'],
-                     env_name=cfg['env']['name'],
-                     video_dir=str(video_dir.absolute()),
-                     video_fps=cfg['logging']['video_fps'],
                      device=device,
+                     normalize_obs=False,
+                     standardization_stats_init_iter=0,
+                     standardize_obs=False,
                      resize_dim=None)
+    
+    video_dir = Path(hydra.core.hydra_config.HydraConfig.get().runtime.output_dir) / Path(cfg['logging']['video_dir'])
+    video_logger = CSVLogger(exp_name=cfg['experiment']['name'], log_dir=video_dir, video_format="mp4", video_fps=cfg['logging']['video_fps'])
+    recorder = VideoRecorder(logger=video_logger, tag="iteration")
+    
+    env.append_transform(recorder)
     
     policy = RandomPolicy(action_spec=env.action_spec)
     
     collector = SyncDataCollector(
         create_env_fn=env,
         policy=policy,
-        total_frames=400,
-        max_frames_per_traj=100,
-        frames_per_batch=200,
-        reset_at_each_iter=False,
+        total_frames=cfg['exploration']['total_frames'],
+        max_frames_per_traj=cfg['exploration']['max_frames_per_traj'],
+        frames_per_batch=cfg['exploration']['frames_per_batch'],
+        reset_at_each_iter=cfg['exploration']['reset_at_each_iter'],
         device=device,
         storing_device=device,
     )
 
-    print("Exploring env...")
+    logger.info("Exploring env...")
     
     for _ in tqdm(collector):
         continue
     
-    print("Done exploring env!")
+    logger.info("Done exploring env!")
     
-    print("Closing env...")
+    logger.info("Saving video...")
     env.transform.dump()
+    logger.info("Closing env...")
     env.close()
 
 if __name__ == "__main__":
